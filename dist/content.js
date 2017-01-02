e303fe80f58891b39c105d0d925ec8209861e04d
Raymond <ray@firestitch.com>
2017-01-02 10:50:31 -0500

    added modal

--- dist/content.js
+++ dist/content.js
@@ -35,6 +35,54 @@
             	});
             }
         };
+    })
+    .directive('fsContentModal', function(contentService, $mdDialog, $sce) {
+        return {
+            template: '<ng-transclude></ng-transclude>',
+            restrict: 'A',
+            transclude: true,
+            scope: {
+            	id: '@fsContentModal',
+            	title: '@?fsTitle'
+            },
+            link: function ($scope, element) {
+
+            	contentService.get($scope.id)
+            	.then(function(content) {
+            		$scope.content = $sce.trustAsHtml(content);
+            	});
+
+            	angular.element(element).on('click',function() {
+
+					var template = '<md-dialog aria-label="Tooltip">\
+					        <md-toolbar ng-show="title">\
+					            <div class="md-toolbar-tools"><h2>{{title}}</h2></div>\
+					        </md-toolbar>\
+					        <md-dialog-content>\
+					            <div class="md-dialog-content" ng-bind-html="content"></div>\
+					        </md-dialog-content>\
+					        <md-dialog-actions>\
+					            <md-button ng-click="close()">Close</md-button>\
+					        </md-dialog-actions>\
+					</md-dialog>';
+
+            		$mdDialog.show({
+	            						template: template,
+	            						controller: ['$scope','content','$mdDialog','title',function($scope, content, $mdDialog, title) {
+	            							$scope.content = content;
+	            							$scope.title = title;
+	            							$scope.close = function() {
+	            								$mdDialog.hide();
+	            							}
+	            						}],
+	            						locals: {
+		            						content: $scope.content,
+		            						title: $scope.title
+		            					}
+	            					});
+            	});
+            }
+        };
     });
 })();
 
